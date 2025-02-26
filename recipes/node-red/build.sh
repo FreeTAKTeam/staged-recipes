da389bdd1d3f792618a4aa4174824df26bfc8a3b
Fredrick Eisele <fred.eisele@vanderbilt.edu>
2025-02-26 10:05:22 -0600

    restore to orginal form

--- recipes/node-red/build.sh
+++ recipes/node-red/build.sh
@@ -1,24 +1,21 @@
 #!/usr/bin/env bash
 
+set
+
 set -o xtrace -o nounset -o pipefail -o errexit
 
-# env
-export SRC_DIR_1=./src/github.com/node-red/node-red
 find .
-mkdir -p ./tgt/
-export TAR_GET=./tgt/${PKG_NAME}-${PKG_VERSION}.tgz
 
-# Create package archive and install globally
-pnpm pack ./src/github.com/node-red/node-red
-pnpm store path
-pnpm store add -ddd \
+npm pack --ignore-scripts
+find .
+npm install -ddd \
     --global \
     --build-from-source \
-    ${TAR_GET}
-    # ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
+    ${SRC_DIR}/${PKG_NAME}-${PKG_VERSION}.tgz
 
+find . 
 # Create license report for dependencies
-pnpm install ${TAR_GET}
+pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
 tee ${PREFIX}/bin/node-red.cmd << EOF
